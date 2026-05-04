a76127eb3c783348e5a34eac88a7593220611aa9
umayer10-AI <mdumayer10@gmail.com>
2026-05-04 18:52:17 +0600

    add

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const http = require('http');
+const server = http.createServer((req,res) => {
+
+    if(req.url === '/'){
+        res.writeHead(200, {'content-type' : 'text/plain'})
+    }
+
+})
+
+server.listen(5000,() => {
+    console.log("Server is running 5000")
+})
